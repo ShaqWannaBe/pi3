d57e43c52054a27b96b6db38a93c7cc616b6437e
Devin Lindsay <devin.lindsay101@gmail.com>
2023-08-29 20:41:24 -0700

    Weather app initial commit

---
+++ weatherApp.py
@@ -0,0 +1,39 @@
+"""
+
+This app will query a weather API to find data about the weather.
+Then it will notify users of the weather.
+
+"""
+
+import os
+from dotenv import load_dotenv
+import requests
+import json
+from colorama import Fore as FORE
+
+load_dotenv() #loads .env file contents
+api_key = os.getenv("API_KEY")
+
+lat = '33.293160'
+lon = '-112.037024'
+units = 'imperial'      # Use either metric or imperial
+
+url = f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&units={units}&exclude=minutely,hourly&appid={api_key}'
+
+res = requests.get(url)
+data = res.json()
+
+#prints the result of the api call
+# print(f'{FORE.RED}{res}{FORE.RESET}')
+
+#round the current temp and turn it into an integer
+temp = int(round(data['current']['temp'],0))
+feels_like = (int(round(data['current']['feels_like'])))
+condition = data['current']['weather']
+
+#print welcome
+print('\n*** WELCOME TO THE WEATHER APP ***')
+print('* Current Temp: ' + str(temp) + ' °F')
+print('* Feels like: ' + str(feels_like) + ' °F')
+
+
